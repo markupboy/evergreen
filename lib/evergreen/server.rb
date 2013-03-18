b18aaee4769ac09bc607092b6cb3a8b1a7ee017f
Blake Walters <blake@markupboy.com>
2013-03-18 10:14:05 -0600

    fixed shutdown for evergreen serve

--- lib/evergreen/server.rb
+++ lib/evergreen/server.rb
@@ -4,6 +4,12 @@ module Evergreen
 
     def serve
       server.boot
+
+      # https://github.com/TLSLLC/evergreen/commit/49808a1827887752302ea2ce1b08b107c9e26174
+      trap(:INT) { puts ""; Kernel.exit(0) }
+      trap(:TERM) { Kernel.exit(0) }
+      trap(:QUIT) { Kernel.exit(0) }
+
       Launchy.open(server.url(Evergreen.mounted_at.to_s + '/'))
       sleep
     end
